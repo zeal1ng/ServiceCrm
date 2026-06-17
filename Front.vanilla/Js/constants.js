74ad8825ecfd09ef040c71c577bb7ba69af259b2
zeal1ng <egoraros75@gmail.com>
2026-06-17 12:16:45 +0300

    Delete Front.vanilla directory

--- Front.vanilla/Js/constants.js
+++
@@ -1,6 +0,0 @@
-export const STATUSES = ['New', 'Diagnostics', 'Repair', 'Ready', 'Issued'];
-export const STATUS_LABELS = { New: 'Новый', Diagnostics: 'На диагностике', Repair: 'В ремонте', Ready: 'Готов', Issued: 'Выдан' };
-export const PRIORITIES = ['Normal', 'High', 'VIP'];
-export const PRIORITY_LABELS = { Normal: 'Обычный', High: 'Срочно', VIP: 'VIP' };
-export const PRIORITY_COLORS = { Normal: '', High: '#ff9800', VIP: '#f44336' };
-export const ROLE_NAMES = { Admin: 'Администратор', Master: 'Мастер', Manager: 'Менеджер' };
